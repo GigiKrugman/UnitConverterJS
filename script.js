885dec3d5a49ac7c38a235bb4f8aaf81613cf41e
Marcello Mulas <marcellomulas93@gmail.com>
2022-09-13 19:22:23 +0200

    Fixed some issue in feat/JS

--- script.js
+++ script.js
@@ -4,21 +4,41 @@ let conversionOne = document.getElementById("conversion-1");
 let conversionTwo = document.getElementById("conversion-2");
 let conversionThree = document.getElementById("conversion-3");
 
-const meterToFeet = 3.281;
-const literToGallon = 0.264;
-const kilogramToPound = 2.204;
+const meterToFeet = (3.281).toFixed(2);
+const feetToMeter = (0.3048).toFixed(2);
+
+const literToGallon = (0.264).toFixed(2);
+const gallonToLiter = (3.785).toFixed(2);
+
+const kilogramToPound = (2.204).toFixed(2);
+const poundToKilogram = (0.453592).toFixed(2);
 
 btnConvert.addEventListener("click", function () {
-  let firstConversion = meterToFeet;
-  let secondConversion = literToGallon;
-  let thirdConversion = kilogramToPound;
+  let firstConversion = inputEl.value;
+  let secondConversion = inputEl.value;
+  let thirdConversion = inputEl.value;
 
   conversionOne.textContent = `1 meter = ${meterToFeet} Feet
-  | ${firstConversion} meters = ${firstConversion * meterToFeet} Feet`;
+  | ${firstConversion} meters = ${(firstConversion * meterToFeet).toFixed(
+    2
+  )} Feet |
+  ${firstConversion} Feet = ${(firstConversion * feetToMeter).toFixed(
+    2
+  )} Meters`;
 
-  conversionTwo.textContent = `1 liter = ${literToGallon} Feet
-  | ${secondConversion} liters = ${secondConversion * literToGallon} Gallons`;
+  conversionTwo.textContent = `1 liter = ${literToGallon} Gallons
+  | ${secondConversion} liters = ${(secondConversion * gallonToLiter).toFixed(
+    2
+  )} Gallons |
+  ${secondConversion} Gallon = ${(firstConversion * gallonToLiter).toFixed(
+    2
+  )} Liters`;
 
-  conversionThree.textContent = `1 Kilogram = ${kilogramToPound} Kilogram
-  | ${thirdConversion} Pounds = ${thirdConversion * kilogramToPound} Feet`;
+  conversionThree.textContent = `1 Kilogram = ${kilogramToPound} Pounds
+  | ${thirdConversion} Pounds = ${(thirdConversion * poundToKilogram).toFixed(
+    2
+  )} Kilograms |
+  ${thirdConversion} Pounds = ${(firstConversion * poundToKilogram).toFixed(
+    2
+  )} Kilograms`;
 });
