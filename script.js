a57758eae1f6e07608a72201fc0a8f28d9d3f7d6
Marcello Mulas <marcellomulas93@gmail.com>
2022-09-13 16:14:32 +0200

    Added feat/JS

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let btnConvert = document.getElementById("btn-convert");
 let inputEl = document.getElementById("input-field");
-let conversionEl = document.getElementById("conversion-p");
+let conversionOne = document.getElementById("conversion-1");
+let conversionTwo = document.getElementById("conversion-2");
+let conversionThree = document.getElementById("conversion-3");
 
 const meterToFeet = 3.281;
 const literToGallon = 0.264;
 const kilogramToPound = 2.204;
 
 btnConvert.addEventListener("click", function () {
-  let startConversion = meterToFeet;
+  let firstConversion = meterToFeet;
+  let secondConversion = literToGallon;
+  let thirdConversion = kilogramToPound;
 
-  conversionEl.textContent = `1 meter = ${meterToFeet} Feet
-  | ${startConversion} meters = ${startConversion * meterToFeet} Feet
-  `;
+  conversionOne.textContent = `1 meter = ${meterToFeet} Feet
+  | ${firstConversion} meters = ${firstConversion * meterToFeet} Feet`;
+
+  conversionTwo.textContent = `1 liter = ${literToGallon} Feet
+  | ${secondConversion} liters = ${secondConversion * literToGallon} Gallons`;
+
+  conversionThree.textContent = `1 Kilogram = ${kilogramToPound} Kilogram
+  | ${thirdConversion} Pounds = ${thirdConversion * kilogramToPound} Feet`;
 });
